7bc1cbe4562daf21028dd87496e0475eecc7ef40
froggy-jpg <meowiemeowie@proton.me>
2024-10-28 20:02:24 +0300

    add nerdfont icons

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ fn main() {
     let packages = froggyfetch::packages::fetch_packages();
 
     println!("╭─────────────────────┬────────╮");
-    println!("│{}│  {}  │ {}", "        _   _        ".green(), "user".green(), user);
-    println!("│{}│  {}  │ {}", "       (.)_(.)       ".green(), "host".green(), hname);
-    println!("│{}│  {}  │ {}", "    _ (   _   ) _    ".green(), "krnl".green(), kernel);
-    println!("│{}│  {}  │ {}", r"   / \/`-----'\/ \   ".green(), " os ".green(), os);
-    println!("│{}│  {}  │ {}", r" __\ ( (     ) ) /__ ".green(), "shll".green(), shell);
+    println!("│{}│ {} │ {}", "        _   _        ".green(), " user".green(), user);
+    println!("│{}│ {} │ {}", "       (.)_(.)       ".green(), "󰇅 host".green(), hname);
+    println!("│{}│ {} │ {}", "    _ (   _   ) _    ".green(), " krnl".green(), kernel);
+    println!("│{}│ {} │ {}", r"   / \/`-----'\/ \   ".green(), "  os ".green(), os);
+    println!("│{}│ {} │ {}", r" __\ ( (     ) ) /__ ".green(), " shll".green(), shell);
     println!("│{}├────────┤", r" )   /\ \._./ /\   ( ".green());
-    println!("│{}│  {}  │ {}", r"  )_/ /|\   /|\ \_(  ".green(), "uptm".green(), uptime);
-    println!("│{}│  {}  │ {}", "                     ".green(), "pkgs".green(), packages);
+    println!("│{}│ {} │ {}", r"  )_/ /|\   /|\ \_(  ".green(), "󰅐 uptm".green(), uptime);
+    println!("│{}│ {} │ {}", "                     ".green(), "󰏖 pkgs".green(), packages);
     println!("╰─────────────────────┴────────╯")
 }
